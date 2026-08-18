e6d3341d8c2ac6606ca8d6ea3d019f882593540b
Kevin Lenzo <kevinlenzo@gmail.com>
2026-08-18 11:01:18 -0400

    Add pstrain.api.corpus and route the split command

---
+++ pstrain/api/corpus.py
@@ -0,0 +1,9 @@
+"""Public API for corpus utilities."""
+
+from pstrain.lib.corpus import split_is_external, train_test_split, validate_external_split
+
+__all__ = [
+    "split_is_external",
+    "train_test_split",
+    "validate_external_split",
+]
